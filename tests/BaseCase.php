3e2740e6dc6f2f00ba550c93938fc92750146d98
Ruslan <abdullin.dev@gmail.com>
2026-05-17 22:47:40 +0500

    поправлен запуск тестов, но остается проблема инициализации kernel в зависимости от env

--- tests/BaseCase.php
+++ tests/BaseCase.php
@@ -6,10 +6,25 @@ namespace App\Tests;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Zenstruck\Foundry\Test\Factories;
+use Zenstruck\Foundry\Test\ResetDatabase;
 
 class BaseCase extends WebTestCase
 {
 
     use Factories;
+    use ResetDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $_ENV['APP_ENV'] = 'test';
+        $_SERVER['APP_ENV'] = 'test';
+
+        self::bootKernel([
+            'environment' => 'test',
+            'debug'       => true,
+        ]);
+    }
 
 }
